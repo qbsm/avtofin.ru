769278decaec4c3252efcc66691eeb067063a7c5
Vyacheslav Fadeev <fadva@mail.ru>
2023-12-20 18:22:29 +0300

    Калькулятор

--- dev/src/components/calc/calc.js
+++ dev/src/components/calc/calc.js
@@ -1,2 +1,49 @@
 export default () => {
+
+    function calc() {
+        const rate = Number($('.js-rate-label').attr('data-rate'));
+        const amount = Number($('.js-amount-range').val());
+        const months = Number($('.js-months-range').val());
+        const amountVal = String(amount).replace(/(.)(?=(\d{3})+$)/g,'$1 ')
+
+        if ($('.js-amount-input').is(":focus")) {
+            $('.js-amount-input').val(amount);
+        } else {
+            $('.js-amount-input').val(amountVal);
+        }
+        $('.js-months-input').val(months);
+        $('.js-months-label').html(months);
+
+        const payment = Math.round((amount + Math.round(amount*(rate/100)*months))/months);
+        const paymentVal = String(payment).replace(/(.)(?=(\d{3})+$)/g,'$1 ')
+        $('.js-payment-label').html(paymentVal);
+    }
+
+    $(document).ready(function() {
+        calc();
+        $('.js-amount-range').on('input', calc).on('change', calc);
+        $('.js-months-range').on('input', calc).on('change', calc);
+        $('.js-amount-input').on('focus', function() {
+            $(this).val($(this).val().replace(' ', ''));
+        });
+        $('.js-amount-input').on('focusout', function() {
+            const amount = $(this).val();
+            if (amount !== '') {
+                $('.js-amount-range').val(amount.replace(' ', '')).change();
+            }
+        });
+        $('.js-amount-input').on('input', function(){
+            const amount = $(this).val();
+            if (amount !== '') {
+                $('.js-amount-range').val(amount.replace(' ', '')).change();
+            }
+        });
+        $('.js-months-input').on('input', function(){
+            const months = $(this).val();
+            if (months !== '') {
+                $('.js-months-range').val(months).change();
+            }
+        });
+    });
+
 }
