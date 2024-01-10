a8ee87a3687ddbf19ebad4477d5a6c78b09cb2a9
Vyacheslav Fadeev <fadva@mail.ru>
2024-01-10 19:50:55 +0500

    Замена изображений в intro по таймеру

--- dev/src/components/intro/intro.js
+++ dev/src/components/intro/intro.js
@@ -1,2 +1,22 @@
 export default () => {
+    function changeImage() {
+        const el = $('.js-img-change');
+        const last = el.attr('data-last');
+        const current = Number(el.attr('data-current'));
+        let next = current + 1;
+        if (next > last) {
+            next = 1;
+        }
+        let src = el.attr('src');
+        src = src.replace(current, next);
+        el.attr('src', src);
+        el.attr('data-current', next);
+    }
+
+    $(document).ready(function () {
+       const delay = $('.js-img-change').attr('data-delay');
+       if (delay) {
+           setInterval(changeImage, Number(delay));
+       }
+    });
 }
