f6d3b21c17b47d97dbe022f1b667229853e31e99
Vyacheslav Fadeev <fadva@mail.ru>
2024-04-04 02:57:49 +0500

    Форма обратной связи разбита на два шага

--- dev/src/components/form/form.js
+++ dev/src/components/form/form.js
@@ -2,6 +2,39 @@
 const Formatter = require('formatter.js/dist/formatter');
 
 export default (opt) => {
+  function validatePhone(phone) {
+    const phoneRegex = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
+    return phoneRegex.test(phone);
+  }
+  function validateName(name) {
+    const nameRegex = /^[A-zА-я -]+$/;
+    return nameRegex.test(name);
+  }
+
+  $('.js-form-next').on('click', function() {
+    const nameCorrect = validateName($('.js-form-name').val());
+    const phoneCorrect = validatePhone($('.js-form-phone').val());
+    if (nameCorrect) {
+      $('.js-form-name-error').addClass('hidden');
+      $('.js-form-name').removeClass('invalid');
+    } else {
+      $('.js-form-name-error').removeClass('hidden');
+      $('.js-form-name').addClass('invalid');
+    }
+    if (phoneCorrect) {
+      $('.js-form-phone-error').addClass('hidden');
+      $('.js-form-phone').removeClass('invalid');
+    } else {
+      $('.js-form-phone-error').removeClass('hidden');
+      $('.js-form-phone').addClass('invalid');
+    }
+    if (!phoneCorrect || !nameCorrect) {
+      return;
+    }
+    $('.js-form-part2').removeClass('hidden');
+    $('.js-form-part1').addClass('hidden');
+  });
+
   $('[name="Телефон"]').on('focus', function handleFocus() {
     const $this = $(this);
     if (!$this.hasClass('formatted')) {
--- project/form.php
+++ project/form.php
@@ -7,6 +7,8 @@ use Ismart\Form\Form;
 $config = (include_once(__DIR__ . "/config.php"));
 $data = $_POST;
 
+file_put_contents('logs/form_'.date('Y-m-d_His').'_data.txt', print_r($data,true));
+
 if (array_key_exists("Телефон", $data)) {
   $phone = trim($_POST["Телефон"]);
 
@@ -21,17 +23,21 @@ if (array_key_exists('city', $data)) {
   $city = $data['city'];
   $data['Филиал'] = $city;
   unset($data['city']);
-  $indexData = json_decode(file_get_contents('data/production/index-production.json'),true);
-  foreach ($indexData['secondaryScreen'] ?? [] as $section) {
-    if (($section['name'] ?? '') == 'branches') {
-      foreach ($section['items'] ?? [] as $item) {
-        if ($item['city']==$city) {
-          $recipients = $item['emailRecipients'];
-          if ($recipients) {
-            $config['mail']['address'] = $recipients;
-          } else {
-            $config['mail']['address'] = ['dev@ismart.pro'];
-            $data['error'] = "Не найдены получатели для филиала  \"$city\"";
+  if ($city == 'Другое') {
+    $config['mail']['address'] = ['autozaimorg@yandex.ru'];
+  } else {
+    $indexData = json_decode(file_get_contents('data/production/index-production.json'), true);
+    foreach ($indexData['secondaryScreen'] ?? [] as $section) {
+      if (($section['name'] ?? '') == 'branches') {
+        foreach ($section['items'] ?? [] as $item) {
+          if ($item['city'] == $city) {
+            $recipients = $item['emailRecipients'];
+            if ($recipients) {
+              $config['mail']['address'] = $recipients;
+            } else {
+              $config['mail']['address'] = ['dev@ismart.pro'];
+              $data['error'] = "Не найдены получатели для филиала  \"$city\"";
+            }
           }
         }
       }
