37e35178f721cd3361886e4192424eeb071e3989
Danil Fedorovich <fedorovich.d@gmail.com>
2024-04-23 01:29:25 +0300

    Доработки и улучшения по ТЗ

--- dev/src/pages/index/index.js
+++ dev/src/pages/index/index.js
@@ -5,7 +5,7 @@ import linkTo from 'modules/link-to';
 import modal from 'modal/modal';
 import form from 'form/form';
 
-import header from 'header/header';import nav from 'nav/nav';import intro from 'intro/intro';import calc from 'calc/calc';import actions from 'actions/actions';import advantages from 'advantages/advantages';import reviews from 'reviews/reviews';import accordion from 'accordion/accordion';import conditions from 'conditions/conditions';import branches from 'branches/branches';import footer from 'footer/footer';
+import header from 'header/header';import nav from 'nav/nav';import intro from 'intro/intro';import advantages from 'advantages/advantages';import calc from 'calc/calc';import actions from 'actions/actions';import reviews from 'reviews/reviews';import accordion from 'accordion/accordion';import conditions from 'conditions/conditions';import branches from 'branches/branches';import footer from 'footer/footer';
 /*eslint-enable */
 
 
@@ -15,5 +15,5 @@ $(document).ready(() => {
   linkTo(0);
   modal();
   form();
-   header(); nav(); intro(); calc(); actions(); advantages(); reviews(); accordion(); conditions(); branches(); footer();
+   header(); nav(); intro(); advantages(); calc(); actions(); reviews(); accordion(); conditions(); branches(); footer();
 });
